432b819c0f2cc5e7066c7c239e3f30c7b1eb8b08
gzu300 <zhuguanchen@me.com>
2020-03-16 16:08:55 +0100

    created design pattern

--- script/modules/ui_new.py
+++ script/modules/ui_new.py
@@ -1,82 +1,47 @@
 #coding=utf-8
+from abc import ABC, abstractclassmethod
 # import sys
 # import pandas as pd
 # from .logic_layer import TeacherMngr, StudentMngr, Operations, AdminMngr
 
 class Base:
-    mngr = None
-    def __init__(self):
-        self._options = {'b': Welcome}
-        self._user_responses = []
-        self._next_scene = None
-        
-    def interact(self):
-        self.show_options()
-        self.ask_user()
-        self.communicate_logiclayer()
-        self.reply_user()
-        
-    def ask_user(self):
-        response = input('Select a cat:').strip().lower()
-        self._next_scene = response
+    def __init__(self, idisplay, igetResponse):
+        self._idisplay = idisplay
+        self._igetResponse = igetResponse
+    def display(self):
+        return self._idisplay.display()
+    def getResponse(self):
+        return self._igetResponse.getResponse()
 
-    def communicate_logiclayer(self):
-        '''
-        logic layer should return the message. No None result accepted
-        '''
-        df = 'a result from logiclayer'
-        print(df)
-        
-    def reply_user(self):
-        scene=self._options.get(self._next_scene)
-        if scene:
-            next_scene = scene()
-            next_scene.interact()
-        
-    def show_options(self):
-        for cat, ui_objs in self._options.items():
-            print(cat, ui_objs.__name__)
-            
-class Operations:
-    def login(self):
-        return
-    def add(self):
-        return
-    def update(self):
-        return
-    def query(self):
-        return
-    def delete(self):
-        return
-            
-class Welcome(Base):
-    def __init__(self):
-        Base.__init__(self)
-        self._options.update({'1': Teacher})
+#Behaviour classes
+class IDisplay(ABC):
+    @abstractclassmethod
+    def display(self):
+        pass
 
-class Lesson(Base):
-    pass
+class IgetResponse(ABC):
+    @abstractclassmethod
+    def getResponse(self):
+        pass
+
+#Parent class for the same categories
+class Actions(IDisplay):
+    '''
+    for add, update, delete and qery concrete classes.
+    '''
+    def display(self):
+        print('concrete display method, in {0} class'.format(self.__class__.__name__))
+    def getResponse(self):
+        print('concrete getResponse method')
+
+#specific concrete classes
+class Add(Actions):
     
-class Course(Base):
-    def __init__(self):
-        Base.__init__(self)
-        self._options.update({'1': 'add'})
-    
-class Teacher(Base):
-    def __init__(self):
-        Base.__init__(self)
-        self._options.update({'1': Course})
-        Base.mngr = 'Teacher'
-    
-class Student(Base):
-    pass
-    
-class Attendance(Base):
-    pass
+
+
     
 if __name__ == '__main__':
-    CATEGORIES = {'Welcome': {'1': Teacher}, 'Teacher': {'1': Course}}
-    # ui = Welcome()
-    # ui.main()
+    add = Base(Add(), Add())
+    add.display()
 
 
